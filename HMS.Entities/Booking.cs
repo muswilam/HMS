078199dd70059d7edb4d6aa280806b1917fa6b30
Sully <jackswilam@gmail.com>
2019-07-04 15:04:51 +0200

    Added Entities

---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime FromDate { get; set; }
+        /// <summary>
+        /// No of stay nights
+        /// </summary>
+        public int Duration { get; set; }
+
+        public int AccommodationId { get; set; }
+        public Accommodation Accommodation { get; set; }
+    }
+}
