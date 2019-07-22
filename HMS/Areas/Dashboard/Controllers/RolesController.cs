38c1a5358d78a5ebfbc8a4c3a8839aa16f4446d5
Sully <jackswilam@gmail.com>
2019-07-22 17:09:01 +0200

    Roles : RoleManager for handling roles table data & Listing & Searching functionality

---
+++ HMS/Areas/Dashboard/Controllers/RolesController.cs
@@ -0,0 +1,112 @@
+﻿using HMS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using HMS.Entities;
+using System.Data.Entity;
+using HMS.Areas.Dashboard.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using HMS.ViewModels;
+
+namespace HMS.Areas.Dashboard.Controllers
+{
+    public class RolesController : Controller
+    {
+        private HMSSignInManager _signInManager;
+        private HMSUserManager _userManager;
+        private HMSRoleManager _roleManager;
+
+        public HMSSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<HMSSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        public HMSUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<HMSUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public HMSRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().GetUserManager<HMSRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+        public RolesController()
+        {
+        }
+
+        public RolesController(HMSUserManager userManager, HMSSignInManager signInManager, HMSRoleManager roleManger)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            RoleManager = roleManger;
+        }
+
+        public ActionResult Index(string searchTerm, int? page)
+        {
+            RolesListingViewModel model = new RolesListingViewModel();
+
+            page = page ?? 1;
+            var pageSize = 10;
+            var totalRecords = GetAllRolesCount(searchTerm);
+
+            model.Roles = SearchRoles(searchTerm, page.Value, pageSize);
+
+            model.SearchByName = searchTerm;
+
+            model.Pager = new Pager(totalRecords, page, pageSize);
+
+            return View(model);
+        }
+
+        //get list of users by search for name or by role
+        public IEnumerable<IdentityRole> SearchRoles(string searchTerm, int page , int pagesize)
+        {
+            var roles = RoleManager.Roles.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                roles = roles.Where(r => r.Name.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            var skip = (page - 1) * pagesize;
+
+            return roles.OrderBy(a => a.Name).Skip(skip).Take(pagesize).ToList();
+        }
+
+        //get total number of records
+        public int GetAllRolesCount(string searchTerm)
+        {
+            var roles = RoleManager.Roles.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                roles = roles.Where(r => r.Name.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            return roles.Count();
+        }
+    }
+}
---
+++ HMS/Areas/Dashboard/ViewModels/RolesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using HMS.ViewModels;
+
+namespace HMS.Areas.Dashboard.ViewModels
+{
+    public class RolesListingViewModel
+    {
+        public IEnumerable<IdentityRole> Roles { get; set; }
+
+        public string SearchByName { get; set; }
+        public Pager Pager { get; set; }
+    }
+
+    public class RolesActionModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
