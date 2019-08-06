7b272fea9d49085f60ea862e3659867c28060448
Sully <jackswilam@gmail.com>
2019-08-06 18:55:48 +0200

    Optimize jQuery code by separate selectPictures code from accPackage and acc to its js file

---
+++ HMS/Areas/Dashboard/Scripts/SelectPictures.js
@@ -0,0 +1,39 @@
+﻿
+$("#selectPictures").change(function () {
+    var pictures = this.files;
+
+    var formData = new FormData();
+    for (var i = 0; i < pictures.length; i++) {
+        formData.append("Picture", pictures[i]);
+    }
+    $.ajax({
+        url: $(this).attr("pics-href"),
+        type: 'Post',
+        data: formData,
+        processData: false,
+        contentType: false
+    })
+    .done(function (result) {
+
+        for (var i = 0; i < result.length; i++) {
+            var picture = result[i];
+
+            var imgHTML = $("#picTemplate").clone();
+            imgHTML.find("img").attr("src", "/images/site/" + picture.URL);
+            imgHTML.find("img").attr("data-id", picture.Id);
+
+            $("#picsArea").append(imgHTML.html());
+        }
+
+        // put all pics ids in hidden input to serialize values to controller 
+        var pictureIds = [];
+
+        $("#picsArea img").each(function () {
+            var picId = $(this).attr("data-id");
+            pictureIds.push(picId);
+        });
+
+        $("#pictureIds").val(pictureIds.join());
+    });
+});
+
